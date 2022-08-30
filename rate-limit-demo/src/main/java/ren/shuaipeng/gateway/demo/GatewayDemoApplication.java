368a0f194c76bd1074696077fc0fa8ed11350d4f
niaoshuai <niao.shuai123@163.com>
2022-08-30 15:23:45 +0800

    Initial Commit

---
+++ rate-limit-demo/src/main/java/ren/shuaipeng/gateway/demo/GatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package ren.shuaipeng.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayDemoApplication.class, args);
+    }
+
+}
---
+++ rate-limit-demo/src/test/java/ren/shuaipeng/gateway/demo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ren.shuaipeng.gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
